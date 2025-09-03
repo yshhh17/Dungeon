bb7c049e4a9001c2120b4be41f0033c1bd11d5c4
Yash <iamyashtiwari28@gmail.com>
2025-09-04 00:17:20 +0530

    added the database files

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime
+from .database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key = True, index = True)
+    username = Column(String, unique = True, index = True, nullable = False)
+    email = Column(String, unique = True, index = True, nullable = False)
+    hashed_password = Column(String, nullable = False)
+    created_at = Column(DateTime, default = datetime.utcnow)
