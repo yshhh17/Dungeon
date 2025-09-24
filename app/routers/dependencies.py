afaf37b4724fd812db2706998c011b5df0d610fc
Yash <iamyashtiwari28@gmail.com>
2025-09-25 00:54:06 +0530

    added the upload and download feature

--- app/routers/dependencies.py
+++ app/routers/dependencies.py
@@ -17,7 +17,7 @@ def get_db():
     finally:
         db.close()
 
-def get_current_user(token: str = Depends(oauth_scheme), db: Session = Depends(get_db)):
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     credentials_exception = HTTPException(
             status_code = status.HTTP_401_UNAUTHORIZED,
             detail = "Could not validate credentials"
--- app/routers/files.py
+++ app/routers/files.py
@@ -4,6 +4,8 @@ from app.database import SessionLocal
 from app import models, schemas
 import shutil
 import os
+from app.routers.dependencies import get_current_user
+from fastapi.responses import FileResponse
 
 router = APIRouter()
 
@@ -19,7 +21,7 @@ UPLOAD_DIR = "uploads/"
 @router.post("/upload", response_model = schemas.FileOut)
 def upload_file(
         file: UploadFile = FastAPIFile(...),
-        owner_id = 1,
+        current_user: models.User = Depends(get_current_user),
         db: Session = Depends(get_db)
 ):
     file_path = os.path.join(UPLOAD_DIR, file.filename)
@@ -28,7 +30,7 @@ def upload_file(
 
 
     db_file = models.File(
-            owner_id = owner_id,
+            owner_id = current_user.id,
             filename = file.filename,
             filepath = file_path
     )
@@ -37,3 +39,22 @@ def upload_file(
     db.refresh(db_file)
 
     return db_file
+
+@router.get("/download/{file_id}")
+def download_file(file_id: int, current_user: models.User = Depends(get_current_user), db: Session = Depends(get_db)):
+
+    db_file = db.query(models.File).filter(models.File.id == file_id).first()
+    if not db_file:
+        raise HTTPException(status_code = 404, detail = "File not Found")
+
+    if db_file.owner_id != current_user.id:
+        raise HTTPException(status_code = 403, detail = "Not authorized to access this file")
+
+    if not os.path.exists(db_file.filepath):
+        raise HTTPException(status_code = 404, detail = "File missing on Server")
+
+    return FileResponse(
+            path = db_file.filepath,
+            filename = db_file.filename,
+            media_type = "application/octet-stream"
+    )
