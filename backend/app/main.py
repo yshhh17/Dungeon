68d16ee30faf9670f2088175f0dddc081446706e
Yash <iamyashtiwari28@gmail.com>
2025-11-22 15:11:44 +0530

    fixed the cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,7 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = ["http://localhost:5173",]
+origins = ["https://dungeon-cloud.vercel.app",]
 
 app.add_middleware(
     CORSMiddleware,
